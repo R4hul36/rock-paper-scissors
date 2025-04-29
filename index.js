980386e4ee420cc8d02407b9631b4346aa50037b
Rahul <rahul.wa47@gmail.com>
2025-04-29 10:06:46 +0530

    fixed the wrong message appearing after each round

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-
-
-
 const getComputerChoice = function () {
-    let noOfChoices = 3
-    let randomNum = Math.floor(Math.random() * noOfChoices)
+  let noOfChoices = 3
+  let randomNum = Math.floor(Math.random() * noOfChoices)
 
-    // below r = rock, p = paper and s = scissors
-    return randomNum === 0 ? "r" : randomNum === 1 ? "p": "s";
-    
+  // below r = rock, p = paper and s = scissors
+  return randomNum === 0 ? 'r' : randomNum === 1 ? 'p' : 's'
 }
 
 const getHumanChoice = function () {
-    userChoice = prompt("Type your choice, rock = r | paper = p | scissors = s");
-    return userChoice;
+  userChoice = prompt('Type your choice, rock = r | paper = p | scissors = s')
+  return userChoice
 }
-
-
 
 const playGame = function () {
-    let humanScore = 0;
-    let computerScore = 0;
-    let rounds = 5;
-    const playGround = function (humanChoice, computerChoice) {
-        // console.log(humanChoice, computerChoice);
-        humanChoice = humanChoice.toLowerCase();
+  let humanScore = 0
+  let computerScore = 0
+  let rounds = 5
+  const playGround = function (humanChoice, computerChoice) {
+    // console.log(humanChoice, computerChoice);
+    humanChoice = humanChoice.toLowerCase()
 
-        if (humanChoice == computerChoice) {
-            return alert("It's a tie");
-        }
-
-        if (humanChoice == "r" && computerChoice == "s" || humanChoice == "p" && computerChoice == "r" || humanChoice == "s" && computerChoice == "p" ) {
-            humanScore +=1;
-            return alert(`player wins! ${humanChoice} beats ${computerChoice}`)
-        }else {
-            computerScore+=1;
-            return alert("Computer wins");
-        }
-
+    if (humanChoice == computerChoice) {
+      return alert("It's a tie")
     }
-    
 
-    for (let i =0; i<rounds; i++) {
-        const computerSelection =getComputerChoice();
-        const humanSelection = getHumanChoice();
-        playGround(humanSelection, computerSelection);
-        
+    if (
+      (humanChoice == 'r' && computerChoice == 's') ||
+      (humanChoice == 'p' && computerChoice == 'r') ||
+      (humanChoice == 's' && computerChoice == 'p')
+    ) {
+      humanScore += 1
+      return alert(`player wins! ${humanChoice} beats ${computerChoice}`)
+    } else {
+      computerScore += 1
+      return alert(`Computer wins! ${computerChoice} beats ${humanChoice}`)
     }
-    if(humanScore > computerScore) {
-        return alert(`player wins! score: player:${humanScore} | computer: ${computerScore}`);
-    }else if (humanScore < computerScore) {
-        return alert(`compuer wins! score: player:${humanScore} | computer: ${computerScore}`);
-    }else {
-        return alert("It's a tie");
-    }
+  }
+
+  for (let i = 0; i < rounds; i++) {
+    const computerSelection = getComputerChoice()
+    const humanSelection = getHumanChoice()
+    playGround(humanSelection, computerSelection)
+  }
+  if (humanScore > computerScore) {
+    return alert(
+      `player wins! player: ${humanScore} | computer: ${computerScore}`
+    )
+  } else if (humanScore < computerScore) {
+    return alert(
+      `compuer wins! player: ${humanScore} | computer: ${computerScore}`
+    )
+  } else {
+    return alert("It's a tie")
+  }
 }
 
-
-playGame();
+playGame()
